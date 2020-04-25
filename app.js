a5cc8dcdf614f51acc07d3df1c514fe43420697e
aTmb405 <aTmb405@gmail.com>
2020-04-25 17:23:35 -0500

    Add static static file config to serve index.html on any route

--- app.js
+++ app.js
@@ -18,10 +18,12 @@ app.use(function (req, res, next) {
     else next();
 });
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '../client/build')));
+-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
