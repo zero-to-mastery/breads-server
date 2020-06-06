7a53ec0904490243999b4e1329b746b27b733aff
aTmb405 <aTmb405@gmail.com>
2020-06-06 01:04:29 -0500

    Update csp report route

--- routes/cspReport.js
+++ routes/cspReport.js
@@ -1,7 +1,7 @@
 let express = require('express'),
     router = express.Router({ mergeParams: true });
 
-router.post('/api/csp-report', (req, res) => {
+router.post('', (req, res) => {
     console.log('CSP header violation', req.body['csp-report'])
     return res.status(204);
 });
