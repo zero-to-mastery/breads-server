193a331bff59bda2e0e424e0a27277108954c31e
aTmb405 <aTmb405@gmail.com>
2020-08-16 12:19:55 -0500

    Return list of reading_ids and user_ids for each tag_id in query

--- controllers/tags.js
+++ controllers/tags.js
@@ -2,7 +2,24 @@ let Tags = require('../models/tags');
 
 exports.findAllTags = async (req, res, next) => {
     try {
-        let tags = await Tags.findAll();
+        let allTags = await Tags.findAll();
+        let tags = allTags.map(tag => {
+            return tag = {
+                'id': tag.id,
+                'tag_name': tag.tag_name,
+                'reading_id': tag.reading_id.split(','),
+                'user_id': tag.user_id.split(','),
+                'date': tag.date,
+                'count': tag.count,
+                // 'favorite': tag.favorite,
+                // 'reader': {
+                //     'id': tag.user_id,
+                //     'username': tag.username,
+                //     'image': tag.image
+                // },
+                // 'tags': tag.tag_ids ? tag.tag_ids.split(',') : null
+            }
+        });
         return res.status(200).json(tags);
     }
     catch (err) {
--- models/tags.js
+++ models/tags.js
@@ -60,7 +60,7 @@ class Tags {
 
     static findAll() {
         let tags = new Promise((resolve, reject) => {
-            db.connection.query('SELECT id, tag_name, reading_tags.reading_id as reading_id, reading_tags.user_id as user_id, created_at as date, count FROM reading_tags LEFT JOIN tags ON tags.id = reading_tags.tag_id ORDER BY reading_tags.reading_id DESC', function(err, results) {
+            db.connection.query('SELECT id, tag_name, GROUP_CONCAT(reading_tags.reading_id) as reading_id, GROUP_CONCAT(reading_tags.user_id) as user_id, created_at as date, count FROM reading_tags LEFT JOIN tags ON tags.id = reading_tags.tag_id GROUP BY id, tag_name, created_at, count ORDER BY created_at DESC;', function(err, results) {
                 if (err) reject(err);
                 else resolve(results);
             });
