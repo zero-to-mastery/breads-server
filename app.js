6e62455c0bfa5f138a978f4269dd7542ceae85de
aTmb405 <aTmb405@gmail.com>
2020-12-16 15:21:06 -0600

    Update cors whitelist

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(helmet.contentSecurityPolicy({
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
-const whitelist = ['https://www.breads.io', 'https://staging-breads-client.herokuapp.com', 'https://jw00ds.github.io/', process.env.LOCAL_CORS];
+const whitelist = ['https://www.breads.io', 'https://staging-breads-client.herokuapp.com', 'https://jw00ds.github.io', process.env.LOCAL_CORS];
 const corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
