9c2a9c2d4915529d22e4a1579968b04b51dee720
aTmb405 <aTmb405@gmail.com>
2021-02-20 00:00:23 -0600

    Update tables

--- mysql/tables.sql
+++ mysql/tables.sql
@@ -19,88 +19,97 @@ ALTER TABLE user_tags DROP FOREIGN KEY user_tags_ibfk_2;
 DROP TABLE IF EXISTS user_tags;
 
 DROP TABLE IF EXISTS tags;
-
 DROP TABLE IF EXISTS readings;
-
 DROP TABLE IF EXISTS users;
 
 -- USERS
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255),
-    image VARCHAR(255),
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-ALTER TABLE users ADD FULLTEXT (first_name, last_name, username);
+  id int(11) NOT NULL AUTO_INCREMENT,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  username varchar(255) NOT NULL,
+  password varchar(255) DEFAULT NULL,
+  image varchar(255) DEFAULT NULL,
+  created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  UNIQUE KEY email (email),
+  UNIQUE KEY username (username),
+  FULLTEXT KEY first_name (first_name,last_name,username)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- READINGS
 CREATE TABLE readings (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    domain VARCHAR(255) NOT NULL,
-    word_count INT NOT NULL,
-    url VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-ALTER TABLE readings ADD COLUMN description varchar(500) AFTER domain;
-ALTER TABLE readings ADD COLUMN image varchar(255) AFTER description;
-ALTER TABLE readings MODIFY image varchar(500);
+  id int(11) NOT NULL AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  domain varchar(255) NOT NULL,
+  description varchar(500) DEFAULT NULL,
+  image varchar(500) DEFAULT NULL,
+  word_count int(11) NOT NULL,
+  url varchar(255) NOT NULL,
+  created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- TAGS
 CREATE TABLE tags (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  tag_name VARCHAR(255) UNIQUE,
-  created_at TIMESTAMP DEFAULT NOW()
-);
-
-ALTER TABLE tags ADD COLUMN count INTEGER DEFAULT 1;
+  id int(11) NOT NULL AUTO_INCREMENT,
+  tag_name varchar(255) DEFAULT NULL,
+  created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  count int(11) DEFAULT 1,
+  PRIMARY KEY (id),
+  UNIQUE KEY tag_name (tag_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- SUBSCRIPTIONS
 CREATE TABLE subscriptions (
-    subscriber_id INT NOT NULL,
-    publisher_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(subscriber_id) REFERENCES users(id),
-    FOREIGN KEY(publisher_id) REFERENCES users(id)
-);
-
-ALTER TABLE subscriptions ADD COLUMN isNew INT DEFAULT 1 AFTER created_at;
+  subscriber_id int(11) NOT NULL,
+  publisher_id int(11) NOT NULL,
+  created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  isNew int(11) DEFAULT 1,
+  PRIMARY KEY (subscriber_id,publisher_id),
+  KEY publisher_id (publisher_id),
+  CONSTRAINT subscriptions_ibfk_1 FOREIGN KEY (subscriber_id) REFERENCES users (id),
+  CONSTRAINT subscriptions_ibfk_2 FOREIGN KEY (publisher_id) REFERENCES users (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- FAVORITES
 CREATE TABLE favorites (
-    user_id INT NOT NULL,
-    reading_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(reading_id) REFERENCES readings(id)
-);
+  user_id int(11) NOT NULL,
+  reading_id int(11) NOT NULL,
+  created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id,reading_id),
+  KEY reading_id (reading_id),
+  CONSTRAINT favorites_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT favorites_ibfk_2 FOREIGN KEY (reading_id) REFERENCES readings (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- READING TAGS
 CREATE TABLE reading_tags (
-    reading_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    FOREIGN KEY(reading_id) REFERENCES readings(id),
-    FOREIGN KEY(tag_id) REFERENCES tags(id)
-);
+  reading_id int(11) NOT NULL,
+  tag_id int(11) NOT NULL,
+  PRIMARY KEY (reading_id,tag_id),
+  KEY tag_id (tag_id),
+  CONSTRAINT reading_tags_ibfk_1 FOREIGN KEY (reading_id) REFERENCES readings (id),
+  CONSTRAINT reading_tags_ibfk_2 FOREIGN KEY (tag_id) REFERENCES tags (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- USER READINGS
 CREATE TABLE user_readings (
-    user_id INT NOT NULL,
-    reading_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(reading_id) REFERENCES readings(id)
-);
+  user_id int(11) NOT NULL,
+  reading_id int(11) NOT NULL,
+  KEY user_id (user_id),
+  KEY reading_id (reading_id),
+  CONSTRAINT user_readings_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT user_readings_ibfk_2 FOREIGN KEY (reading_id) REFERENCES readings (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- USER TAGS
 CREATE TABLE user_tags (
-    user_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(tag_id) REFERENCES tags(id)
-);
+  user_id int(11) NOT NULL,
+  tag_id int(11) NOT NULL,
+  KEY user_id (user_id),
+  KEY tag_id (tag_id),
+  CONSTRAINT user_tags_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT user_tags_ibfk_2 FOREIGN KEY (tag_id) REFERENCES tags (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
