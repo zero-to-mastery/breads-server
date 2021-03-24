b1dc21d50457ef81ae6a7bb5dedc3db48f96c4cc
Max Wood <max.w29@hotmail.com>
2021-03-24 13:59:09 -0500

    backed up the rest of the files

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const localSetup = {
     connectionLimit: 25,
     host: process.env.HOST || process.env.LOCAL_HOST,
-    user: process.env.USERNAME || process.env.LOCAL_USER,
-    password: process.env.DBPASSWORD || process.env.LOCAL_DBPASSWORD,
+    user: "root",
+    password: "password",
     database: process.env.DB || process.env.LOCAL_DB
 }
 
